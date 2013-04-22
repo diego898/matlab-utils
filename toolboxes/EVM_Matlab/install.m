72be4a0b1fa628e36bb6ac85b9dcf474d7723df6
Diego Mesa <damesa@eng.ucsd.edu>
2013-04-22 23:28:47 +0100

    further

--- toolboxes/EVM_Matlab/install.m
+++
@@ -1,2 +0,0 @@
-addpath('./matlabPyrTools');
-addpath('./matlabPyrTools/MEX');
--- toolboxes/EVM_Matlab/make.m
+++
@@ -1,5 +0,0 @@
-
-% Build matlabPyrTools
-fprintf('Building matlabPyrTools...\n');
-run(fullfile('matlabPyrTools', 'Mex', 'compilePyrTools.m'));
-
